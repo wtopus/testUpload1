47cc5c0e350892a17219b521ae50ad5270f30262
wtopus <cake3717@gmail.com>
2022-07-29 04:32:03 +0900

    zNumber update

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ import { utilAes256, utilSha256, goSubmit, randomMchtTrdNo} from '../components/
 //setZNumber1(zNumber1+1);
 //e.target.className = `bg-yellow-100 absolute top-[50%] left-[10%] ` + zNumber1;
 //console.log(e.target.className)
-let test = 0;
+let test = -1;
 export default function Home() {
   
   const [zNumber1, setZNumber1] = useState();
@@ -71,7 +71,7 @@ export default function Home() {
 
   return (
     <>
-      <div id="fffff">{zNumber1}</div>
+      
       <Topnav_hecto />
 
       <div className='flex mt-12 bg-white'>
